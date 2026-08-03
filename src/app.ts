5f7348ed73435b51f49e6e75dc7d5ebb175a374a
Marjan <marjan.posao@gmail.com>
2026-08-03 05:07:34 +0200

    Register call leads API route

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ import { systemRoute } from './routes/system.route.js';
 import { adminRoute } from './routes/admin.route.js';
 import { knowledgeRoute } from './routes/knowledge.route.js';
 import { websiteImportRoute } from './routes/website-import.route.js';
+import { callLeadsRoute } from './routes/call-leads.route.js';
 import { browserVoiceRoute } from './websocket/browser-voice.route.js';
 import { initializeStorage } from './storage-postgres.js';
 
@@ -35,6 +36,7 @@ export async function buildApp() {
   await app.register(adminRoute);
   await app.register(knowledgeRoute);
   await app.register(websiteImportRoute);
+  await app.register(callLeadsRoute);
   await app.register(browserVoiceRoute);
 
   app.setNotFoundHandler(async (_request, reply) => {

47726e083dc1085803f8939608471e8272669983
Marjan <marjan.posao@gmail.com>
2026-09-06 14:12:48 +0200

    Register client activity route

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,7 @@ import { callLeadsRoute } from './routes/call-leads.route.js';
 import { voiceGatewayRoute } from './routes/voice-gateway.route.js';
 import { clientProfileBuilderRoute } from './routes/client-profile-builder.route.js';
 import { clientEditorRoute } from './routes/client-editor.route.js';
+import { clientActivityRoute } from './routes/client-activity.route.js';
 import { browserVoiceRoute } from './websocket/browser-voice.route.js';
 import { initializeStorage } from './storage-postgres.js';
 
@@ -43,6 +44,7 @@ export async function buildApp() {
   await app.register(voiceGatewayRoute);
   await app.register(clientProfileBuilderRoute);
   await app.register(clientEditorRoute);
+  await app.register(clientActivityRoute);
   await app.register(browserVoiceRoute);
 
   app.setNotFoundHandler(async (_request, reply) => {

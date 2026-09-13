3177c9b5a462e4d4ef27091bc942da2433472727
Marjan <marjan.posao@gmail.com>
2026-09-13 02:22:50 +0200

    Register admin PWA route

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,7 @@ import { clientProfileBuilderRoute } from './routes/client-profile-builder.route
 import { clientEditorRoute } from './routes/client-editor.route.js';
 import { clientActivityRoute } from './routes/client-activity.route.js';
 import { telephonySetupRoute } from './routes/telephony-setup.route.js';
+import { adminPwaRoute } from './routes/admin-pwa.route.js';
 import { browserVoiceRoute } from './websocket/browser-voice.route.js';
 import { initializeStorage } from './storage-postgres.js';
 
@@ -47,6 +48,7 @@ export async function buildApp() {
   await app.register(clientEditorRoute);
   await app.register(clientActivityRoute);
   await app.register(telephonySetupRoute);
+  await app.register(adminPwaRoute);
   await app.register(browserVoiceRoute);
 
   app.setNotFoundHandler(async (_request, reply) => {
